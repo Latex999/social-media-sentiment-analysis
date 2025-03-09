e7189f21146a6ae288b1e74e9b1044a97ab777cb
Latex <M.Hassan17877@student.aast.edu>
2025-03-09 22:32:42 +0200

    Initialize models package

---
+++ src/models/__init__.py
@@ -0,0 +1,6 @@
+"""Sentiment analysis models."""
+
+from src.models.analyzer import SentimentAnalyzer
+from src.models.result import SentimentResult, SentimentResultSet
+
+__all__ = ['SentimentAnalyzer', 'SentimentResult', 'SentimentResultSet']
