518e65b9247e47464b292fbe7bba0aa94b4f1f69
Latex <M.Hassan17877@student.aast.edu>
2025-03-09 22:29:12 +0200

    Initialize src package

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Social Media Sentiment Analysis
+===============================
+
+A professional tool for analyzing sentiment in social media posts.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Latex999'
